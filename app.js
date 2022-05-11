189101908e84c9eee1ae45e7a6f320f4b06ba163
Fenwen Huang <pinkwen20@yahoo.com.tw>
2022-05-11 13:00:03 +0800

    新增模組 、補充coding style

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const hello=require('./hello');
+
+// 第一個區塊 內建模組
+const path = require('path');
+const http = require('http');
+
+// 第二個區塊 第三方模組(套件)
+
+// 第三個區塊 自建模組
+const hello = require('./hello');
+
+////////////////////////////////////////////////////////////////
+
 hello.sayHello();
 
 console.log(hello.title);
+
+// app.js
+const cowsay = require('cowsay');
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
