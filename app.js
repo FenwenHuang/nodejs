4ee03896531916e0f54b23e9a0bba7c39d47097f
Fenwen Huang <pinkwen20@yahoo.com.tw>
2022-05-12 10:46:18 +0800

    製作 login 頁面並處理 POST request（第三方套件 body-parser）

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 // 第二個區塊 第三方模組(套件)
 
 const express = require('express');
+const bodyParser = require('body-parser');
 // 第三個區塊 自建模組
 // const hello = require('./hello');
 
@@ -42,6 +43,7 @@ const app = express();
 
 //middleware 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req,res,next)=> {
     console.log('Hello!');
@@ -59,6 +61,14 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.status(200)
         .sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+        res.redirect('/');
+    } else {
+        console.log('欄位尚未填寫完成！')
+    }
 });
 
 app.listen(3000, () => {
