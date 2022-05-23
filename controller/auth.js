d7bc439d6abf4af562870e74aba68b193df215e9
Fenwen Huang <pinkwen20@yahoo.com.tw>
2022-05-23 14:36:50 +0800

    購物車功能(1) 建立 User 時一併建立專屬的 Cart

--- controller/auth.js
+++ controller/auth.js
@@ -1,36 +1,35 @@
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
-
-const getLogin=(req, res) => {
-    const errorMessage = req.flash('errorMessage')[0];
-    res.status(200).render('auth/login',{
-            pageTitle: 'Login',
-            errorMessage
-    });
-}
-
-const getSignup=(req,res)=>{
+////////////////////////////////////////////////////////////////
+const getLogin = (req, res) => {
     const errorMessage = req.flash('errorMessage')[0];
     res.status(200)
-    .render('auth/signup',{
-        pageTitle:'Signup',
-        errorMessage
-    })
+        .render('auth/login', {
+            pageTitle: 'Login',
+            errorMessage
+        });
+};
+const getSignup = (req, res) => {
+    const errorMessage = req.flash('errorMessage')[0];
+    res.status(200)
+        .render('auth/signup', {
+            pageTitle: 'Signup',
+            errorMessage
+        });
 }
-
-
-const postLogin=(req, res) => {
+const postLogin = (req, res) => {
     const { email, password } = req.body;
-
     User.findOne({ where: { email }})
-.then((user) => {
-    if (!user) {
-       req.flash('errorMessage', '錯誤的 Email 或 Password。');
-        return res.redirect('/login');
-    }
-    bcryptjs
+        .then((user) => {
+            console.log('user', user);
+            if (!user) {
+                req.flash('errorMessage', '錯誤的 Email 或 Password。')
+                return res.redirect('/login');
+            }
+            bcryptjs
                 .compare(password, user.password)
                 .then((isMatch) => {
+                    console.log('isMatch', isMatch);
                     if (isMatch) {
                         req.session.user = user;
                         req.session.isLogin = true;
@@ -46,50 +45,52 @@ const postLogin=(req, res) => {
                     return res.redirect('/login');
                 })
         })
-.catch((err) => {
-    console.log('login error:', err);
-
-});
+        .catch((err) => {
+            console.log('login error:', err);
+        });
 };
-
 const postSignup = (req, res) => {
     const { displayName, email, password } = req.body;
     User.findOne({ where: { email } })
         .then((user) => {
-            console.log('find user: ', user);
             if (user) {
                 req.flash('errorMessage', '此帳號已存在！請使用其他 Email。')
                 return res.redirect('/signup');
-            } else {
-                return bcryptjs.hash(password, 12)
-                    .then((hashedPassword) => {
-                        return User.create({ displayName, email, password: hashedPassword });
-                    })
-                    .catch((err) => {
-                        console.log('create new user error: ', err);
+             } else {
+                 return bcryptjs.hash(password, 12)
+                     .then((hashedPassword) => {
+                         return User
+                             .create({ displayName, email, password: hashedPassword })
+                             .then((newUser) => {
+                                 return newUser.createCart();
+                             })
+                             .catch((err) => {
+                                 console.log('postSignup - newUser.carateCart error: ', err);
+                             });
+                     })
+                     .catch((err) => {
+                         console.log('create new user error: ', err);
                     })
             }
         })
         .then((result) => {
             res.redirect('/login');
         })
-        .catch((err) => {
-            console.log('signup_error', err);
-        });
-}
+         .catch((err) => {
+             console.log('signup_error', err);
+         });
+ };
 
-const postLogout = (req, res) => {
-    req.session.destroy((err) => {
-        console.log('session destroy() error: ', err);
-        res.redirect('/login');
-    });
-};
+ const postLogout = (req, res) => {
+     req.session.destroy((err) => {
+         res.redirect('/login')
+     });
+ };
 
-
-module.exports={
-    getLogin,
+ module.exports = {
+     getLogin,
+    getSignup,
     postLogin,
     postLogout,
-    getSignup,
-    postSignup,
-}
+    postSignup
+};
